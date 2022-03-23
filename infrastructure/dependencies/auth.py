2ed2cff14fc2bc7d74beb14a9bcb917bfb3550ac
StephanYorchenko <yurchenko_s@tochka.com>
2022-03-23 23:14:21 +0300

    авторизация по jwt

--- infrastructure/dependencies/auth.py
+++ infrastructure/dependencies/auth.py
@@ -1,6 +1,9 @@
-from fastapi import Header
+from fastapi import Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
 
 
-async def get_user_from_token(header=Header('', alias='Authorization'), ):
-    token = header.replace('Bearer ', '')
-    return token
+async def get_user_from_token(credentials: HTTPAuthorizationCredentials = Security(security)):
+    # здесь нужна валидация токена
+    return credentials.credentials
