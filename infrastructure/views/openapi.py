0906e05e00112966dedbe5a1f4832984334e34fc
Alexander Lepinskikh <quantumdragon2902@gmail.com>
2022-03-25 19:14:12 +0300

    Requests

--- infrastructure/views/openapi.py
+++ infrastructure/views/openapi.py
@@ -80,8 +80,54 @@ def exchange_code(code: str) -> Dict:
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
     return response.json()
+
+
+def make_payment(access_token: str) -> Dict:
+    url = url = f'{RS_PS_URL}/payments'
+
+    payload = json.dumps({
+        "Data": {
+            "consentId": "85aa9fa5-f468-4767-92e7-c636e8b37ddd",
+            "Initiation": {
+                "instructionIdentification": "PISP412",
+                "endToEndIdentification": "MERCHANT.256702.IDN.12",
+                "InstructedAmount": {
+                    "amount": "1000.00",
+                    "currency": "RUB"
+                },
+                "CreditorAccount": {
+                    "schemeName": "RU.CBR.PAN",
+                    "identification": "40817",
+                    "name": "mvideo"
+                },
+                "RemittanceInformation": {
+                    "unstructured": "Dishwasher machine",
+                    "reference": "Internal operation code 5678"
+                }
+            }
+        },
+        "Risk": {}
+    })
+
+    jws = '.'.join([
+        'eyJhbGciOiJSUzI1NiIsImtpZCI6IjY2NjVCRDA0QUQ0M0JENzY1RDczNDhCNkM3QTdEODI0MTFBMEQ0RTkiLCJ4NXQiOiJabVc5QksxRHZYWmRjMGkyeDZmWUpCR2cxT2siLCJ0eXAiOiJKV1QifQ',
+        b64encode(payload.encode()).decode('utf-8-sig'),
+        'JzPOCXZQthjUzburLxbJVSDPIMB_3SSKhk1jbp0koBsRHXA2S0W0EuYFKKZdqPpVFrwmwZQQ2_tXCjZubSvsGbmEAQjWAKLH1Kd7dLTKzJFKHezWcRI4ljxA-Zvzm5qryX_95KMLoIfvlMMEqhLimXV_zHkvbzFaQwpjRqvfp53ePAbDGa5Hl7PQPOXkGvS-EUal6AEGcjtajyb5ItnA9FcYkk61-QoP-E-Hw6dq6j3YJrqFk0Pygi9D3G1Cxmkii9Vpr1JXCH2B6ah5d5M2kd9lE_-V4a8lgZgpyszwC17ZF_Dr-puLyNrHQzmBuGF4_d4Hnk2TdUkhboFLi6T_SQ'
+    ])
+
+    headers = {
+        'x-idempotency-key': 'd9e45326-ed69-49ea-8015-e9694afc675b',
+        'Accept': 'application/json',
+        'x-jws-signature': jws,
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+
+    resp = requests.post(url, headers=headers, data=payload, verify=False)
+    decoded_data = resp.text.encode().decode('utf-8-sig')
+    return json.loads(decoded_data)
 
 
 @openapi_routes.get('/get_consent')
